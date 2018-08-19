2c8ed7260906ec0698993b7d7226bbd6ffdf362a
Rodrigo Anjos <rodrigo.kaos@ymail.com>
2018-08-19 13:54:37 -0300

    fix remove old html_action

--- functions.php
+++ functions.php
@@ -5,27 +5,6 @@ function chinese_encode( $str ){
 }
 
 function html_action( $arr ){
-	$html  = html_span( $arr['id'] );
-	$html .= html_span( $arr['param'], 'bold' );
-	
-	$url_next = "taskview.php?task=" . $arr['idNext'];
-	$url_fail = "taskview.php?task=" . $arr['idNextFail'];
-
-	$ancor_next = html_ancor( $arr['idNext'], $url_next ); 
-	$ancor_fail = html_ancor( $arr['idNextFail'], $url_fail ); 
-	
-	$div = "<div>" .
-				html_span( "Next: " . $ancor_next ) .
-				html_span( "Fail: " . $ancor_fail ) .
-				html_span( "Data: " . $arr['data']) .
-			"</div>";
-
-	$html .= $div;
-
-	return $html;
-}
-
-function html_action2( $arr ){
 	$html  = html_span( $arr->id );
 	$html .= html_span( chinese_encode( $arr->param ), 'bold' );
 	
--- taskview.php
+++ taskview.php
@@ -8,26 +8,14 @@
  <body>
 
 <?php 
-	require_once('functions.php');
 
-	// $con = new PDO("mysql:host=localhost;dbname=my", "root", "");
-	// $con->exec("set names utf8");
-	
-	// $action_arr = array();
-	// $query = "SELECT * FROM cq_action WHERE id = ? LIMIT 1";
-	// $stmt = $con->prepare( $query );
-	
-	// if ( $stmt->execute( array($_GET['task']) )) {		
-	// 	$action = $stmt->fetch(PDO::FETCH_OBJ);
-	// }
+require_once('functions.php');
 
-	// $html = html_action2($action);
-	// echo '<div>' . $html . '</div>';	
+$arr = get_action( $_GET['task'] );
 
-$arr = get_action($_GET['task']);	
-print_r($arr);
-
-// $actions[] = $action_arr;
+foreach ( $arr as $key => $value ) {
+	echo '<div>' . html_action( $value ) . '</div>';
+}
 	
 function get_action( $action_id, $action_arr = array(), $con = null, $i = 0 ){
 	
@@ -37,24 +25,24 @@ function get_action( $action_id, $action_arr = array(), $con = null, $i = 0 ){
 	}
 
 	$query = "SELECT * FROM cq_action WHERE id = ? LIMIT 1";
-	$stmt = $con->prepare( $query );
-	
+	$stmt = $con->prepare( $query );	
 
 	if ( $stmt->execute( array($action_id) )) {
 		$action = $stmt->fetch( PDO::FETCH_OBJ );
-		$action_arr[ $i ] = $action;		
-			
-		if( $action->id_next != "0000"){
-			if( ! is_indexed( $action->id_next, $action_arr ) )
-					$action_arr = get_action( $action->id_next, $action_arr, $con, $i+1 );
-		}
+		
+		if( $action != false ){
+			$action_arr[ $i ] = $action;
 
-		if( $action->id_nextfail != "0000" ){
-			if( ! is_indexed( $action->id_nextfail, $action_arr ) )
-					$action_arr = get_action( $action->id_nextfail, $action_arr, $con, $i+1 );
-		}
-
+			if( $action->id_next != "0000")
+				if( ! is_indexed( $action->id_next, $action_arr ) )
+						$action_arr = get_action( $action->id_next, $action_arr, $con, $i+1 );
 			
+
+			if( $action->id_nextfail != "0000" )
+				if( ! is_indexed( $action->id_nextfail, $action_arr ) )
+						$action_arr = get_action( $action->id_nextfail, $action_arr, $con, $i+1 );
+			
+		}			
 	}
 
 	return $action_arr;	
